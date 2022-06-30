75ab1a5e36ad09b6083e358406fc83f97a7a8e00
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-06-30 00:07:59 -0300

    feat: add base layout profile fragment

--- app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/events/EventsListFragment.kt
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/events/EventsListFragment.kt
@@ -1,12 +1,8 @@
 package com.github.henriquechsf.syscredentialapp.ui.events
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.github.henriquechsf.syscredentialapp.R
 import com.github.henriquechsf.syscredentialapp.databinding.FragmentEventsListBinding
 import com.github.henriquechsf.syscredentialapp.ui.base.BaseFragment
 
---
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.henriquechsf.syscredentialapp.ui.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+
+}
