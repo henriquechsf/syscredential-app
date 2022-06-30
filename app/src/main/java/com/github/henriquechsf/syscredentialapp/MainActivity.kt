3761bb47839b5e85c86a0ce4e0c83138af4322da
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-06-30 01:22:45 -0300

    feat: add bottom navigation

--- app/src/main/java/com/github/henriquechsf/syscredentialapp/MainActivity.kt
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.github.henriquechsf.syscredentialapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.github.henriquechsf.syscredentialapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener { }
+        }
     }
 }
