637a17df585a9aa8b6eda380d7b0a12627c2e113
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-17 16:03:00 -0300

    bind values on events detail

--- app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/event_detail/EventDetailFragment.kt
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/event_detail/EventDetailFragment.kt
@@ -1,8 +1,12 @@
 package com.github.henriquechsf.syscredentialapp.ui.event_detail
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.github.henriquechsf.syscredentialapp.data.model.Event
 import com.github.henriquechsf.syscredentialapp.databinding.FragmentEventDetailBinding
 import com.github.henriquechsf.syscredentialapp.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,7 +14,24 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class EventDetailFragment : BaseFragment<FragmentEventDetailBinding, EventDetailViewModel>() {
 
+    private val args: EventDetailFragmentArgs by navArgs()
+    private lateinit var event: Event
+
     override val viewModel: EventDetailViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        event = args.event
+        bindingEventValues()
+    }
+
+    private fun bindingEventValues() = with(binding) {
+        tvEventTitle.text = event.title
+        tvEventDescription.text = event.description
+        tvDatetime.text = event.datetime.toString()
+        tvLocation.text = event.local
+    }
 
     override fun getViewBinding(
         inflater: LayoutInflater,
