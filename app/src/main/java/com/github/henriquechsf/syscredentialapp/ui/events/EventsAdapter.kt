f1eec9314c3f61c23fa9752204e185565aee0144
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-17 09:42:30 -0300

    event list adapter

---
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/events/EventsAdapter.kt
@@ -0,0 +1,67 @@
+package com.github.henriquechsf.syscredentialapp.ui.events
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.github.henriquechsf.syscredentialapp.data.model.Event
+import com.github.henriquechsf.syscredentialapp.databinding.ItemEventListBinding
+
+class EventsAdapter : RecyclerView.Adapter<EventsAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: ItemEventListBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Event>() {
+        override fun areItemsTheSame(oldItem: Event, newItem: Event): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.title == newItem.title &&
+                    oldItem.description == newItem.description &&
+                    oldItem.local == newItem.local &&
+                    oldItem.datetime == newItem.datetime
+        }
+
+        override fun areContentsTheSame(oldItem: Event, newItem: Event): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, differCallback)
+
+    var events: List<Event>
+        get() = differ.currentList
+        set(value) = differ.submitList(value)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemEventListBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = events.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val event = events[position]
+        holder.binding.apply {
+            tvEventTitle.text = event.title
+            tvEventDate.text = event.datetime.toString()
+            tvEventLocation.text = event.local
+        }
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.let { clicked ->
+                clicked(event)
+            }
+        }
+    }
+
+    private var onItemClickListener: ((Event) -> Unit)? = null
+    fun setOnClickListener(listener: (Event) -> Unit) {
+        onItemClickListener = listener
+    }
+}
