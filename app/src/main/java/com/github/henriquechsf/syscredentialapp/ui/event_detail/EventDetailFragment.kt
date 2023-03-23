f6555ba4ac1aae27f4e9f5c890adf653d9791388
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-22 23:31:53 -0300

    add icon button in toolbar

--- app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/event_detail/EventDetailFragment.kt
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/event_detail/EventDetailFragment.kt
@@ -2,6 +2,9 @@ package com.github.henriquechsf.syscredentialapp.ui.event_detail
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
@@ -11,7 +14,7 @@ import com.github.henriquechsf.syscredentialapp.R
 import com.github.henriquechsf.syscredentialapp.data.model.Event
 import com.github.henriquechsf.syscredentialapp.databinding.FragmentEventDetailBinding
 import com.github.henriquechsf.syscredentialapp.ui.base.BaseFragment
-import com.github.henriquechsf.syscredentialapp.ui.persons.PersonsListFragment
+import com.github.henriquechsf.syscredentialapp.util.toast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,11 +25,35 @@ class EventDetailFragment : BaseFragment<FragmentEventDetailBinding, EventDetail
 
     override val viewModel: EventDetailViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         event = args.event
         bindingEventData()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_event_detail, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_edit_event -> {
+                findNavController().navigate(R.id.action_eventDetailFragment_to_eventFormFragment)
+                true
+            }
+            R.id.menu_remove_event -> {
+                toast("Remove Event")
+                findNavController().popBackStack()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun bindingEventData() = with(binding) {
