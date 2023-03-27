0c30d99d59f0cbfaabe3f5503686602108b5f9c3
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-27 17:23:30 -0300

    create entity event registration on database

--- app/src/main/java/com/github/henriquechsf/syscredentialapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/data/local/AppDatabase.kt
@@ -3,10 +3,16 @@ package com.github.henriquechsf.syscredentialapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.github.henriquechsf.syscredentialapp.data.model.Event
+import com.github.henriquechsf.syscredentialapp.data.model.EventRegistration
 import com.github.henriquechsf.syscredentialapp.data.model.Person
 
-@Database(entities = [Event::class, Person::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Event::class, Person::class, EventRegistration::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun eventDao(): EventDao
     abstract fun personDao(): PersonDao
+    abstract fun eventRegistrationDao(): EventRegistrationDao
 }
