1d688d6347bc5eedd09b770b2e0afc34b67026c5
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-27 23:35:53 -0300

    open and capture value on code scanner activity

--- app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/code_scanner/CodeScannerViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.github.henriquechsf.syscredentialapp.ui.code_scanner
-
-import androidx.lifecycle.ViewModel
-
-class CodeScannerViewModel : ViewModel() {
-}
--- app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/event_detail/RegistrationListFragment.kt
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/ui/event_detail/RegistrationListFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.github.henriquechsf.syscredentialapp.R
 import com.github.henriquechsf.syscredentialapp.databinding.FragmentRegistrationListBinding
 import com.github.henriquechsf.syscredentialapp.ui.base.BaseFragment
+import com.github.henriquechsf.syscredentialapp.ui.code_scanner.CaptureAct
+import com.github.henriquechsf.syscredentialapp.util.toast
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
 
 class RegistrationListFragment :
     BaseFragment<FragmentRegistrationListBinding, RegistrationListViewModel>() {
@@ -21,17 +27,38 @@ class RegistrationListFragment :
         initListeners()
     }
 
-    private fun initListeners() = with(binding) {
-        binding.fabQrcodeScan.setOnClickListener {
-            findNavController().navigate(R.id.action_registrationListFragment_to_codeScannerFragment)
-        }
-    }
-
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentRegistrationListBinding =
         FragmentRegistrationListBinding.inflate(inflater, container, false)
+
+    private fun initListeners() = with(binding) {
+        binding.fabQrcodeScan.setOnClickListener {
+            //findNavController().navigate(R.id.action_registrationListFragment_to_codeScannerFragment)
+            scanCode()
+        }
+    }
+
+    private fun scanCode() {
+        val options = ScanOptions().apply {
+            setPrompt(getString(R.string.volume_up_flash_scan))
+            setBeepEnabled(true)
+            setOrientationLocked(true)
+            captureActivity = CaptureAct::class.java
+        }
+        barcodeLauncher.launch(options)
+    }
+
+    private val barcodeLauncher = registerForActivityResult<ScanOptions, ScanIntentResult>(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents != null) {
+            toast("Scanned: ${result.contents}", Toast.LENGTH_LONG)
+        } else {
+            toast("Cancelled", Toast.LENGTH_LONG)
+        }
+    }
 
 
 }
