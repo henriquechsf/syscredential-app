b04cf641b52ca1db836d6a954ddb53e98a793e66
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-08-25 14:57:55 -0300

    chore: install firebase auth dependency

---
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/util/FirebaseHelper.kt
@@ -0,0 +1,32 @@
+package com.github.henriquechsf.syscredentialapp.util
+
+import com.github.henriquechsf.syscredentialapp.R
+import com.google.firebase.auth.FirebaseAuth
+
+// TODO: Mover implementacao para classe repository
+class FirebaseHelper {
+
+    companion object {
+        fun isAuthenticated(): Boolean = FirebaseAuth.getInstance().currentUser != null
+
+        fun getUserId() = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+
+        fun getAuth() = FirebaseAuth.getInstance()
+
+        // TODO: Adicionar demais mapeamentos de erro Firebase
+        fun validError(error: String): Int {
+            return when {
+                error.contains("There is no user record corresponding to this identifier.") -> {
+                    R.string.account_not_registered_register_fragment
+                }
+                error.contains("The email address is badly formatted.") -> {
+                    R.string.invalid_email_or_password_register_fragment
+                }
+                error.contains("The password is invalid or the user does not have a password.") -> {
+                    R.string.invalid_email_or_password_register_fragment
+                }
+                else -> R.string.common_error_message_generic
+            }
+        }
+    }
+}
