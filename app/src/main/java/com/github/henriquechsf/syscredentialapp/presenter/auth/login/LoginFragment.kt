bceb98032716b23f073771f3e91741625505fdb5
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-08-25 22:45:51 -0300

    feat: user sign in

--- app/src/main/java/com/github/henriquechsf/syscredentialapp/presenter/auth/login/LoginFragment.kt
+++ app/src/main/java/com/github/henriquechsf/syscredentialapp/presenter/auth/login/LoginFragment.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.github.henriquechsf.syscredentialapp.R
 import com.github.henriquechsf.syscredentialapp.databinding.FragmentLoginBinding
 import com.github.henriquechsf.syscredentialapp.presenter.base.BaseFragment
+import com.github.henriquechsf.syscredentialapp.presenter.base.StateView
+import com.github.henriquechsf.syscredentialapp.util.FirebaseHelper
+import com.github.henriquechsf.syscredentialapp.util.toast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>() {
 
-    private val viewModel: LoginViewModel by viewModels()
+    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?) =
         FragmentLoginBinding.inflate(inflater, container, false)
@@ -26,11 +30,44 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
     }
 
     private fun initListeners() = with(binding) {
+        btnEnter.setOnClickListener {
+            validateData()
+        }
         btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
         btnForgotAccount.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_forgotFragment)
+        }
+    }
+
+    private fun validateData() = with(binding) {
+        val email = edtEmail.text.toString().trim()
+        val password = edtPassword.text.toString().trim()
+
+        if (email.isNotEmpty() || password.isNotEmpty()) {
+            loginUser(email, password)
+        } else {
+            toast(message = "Digite todos os campos")
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        loginViewModel.login(email, password).observe(viewLifecycleOwner) { stateView ->
+            when (stateView) {
+                is StateView.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+                is StateView.Sucess -> {
+                    binding.progressBar.isVisible = false
+
+                    findNavController().navigate(R.id.action_global_profileFragment)
+                }
+                is StateView.Error -> {
+                    binding.progressBar.isVisible = false
+                    toast(message = getString(FirebaseHelper.validError(stateView.message ?: "")))
+                }
+            }
         }
     }
 }
